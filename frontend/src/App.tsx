4eef29a2523d1d79fd312c00355c9f8732fea336
DanSpicy25 <Silvio2504036@gmail.com>
2026-09-21 20:06:21 -0400

    Add future prop to RouterProvider in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,12 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
 export const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider
+      router={router}
+      future={{ v7_startTransition: true }}
+    />
+  );
 };
 
 export default App;
-
